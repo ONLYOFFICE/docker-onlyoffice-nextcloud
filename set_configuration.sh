6461ea02e9b13d4753088d3918c40d4f69831564
Alexey Bannov <alexey.bannov@onlyoffice.com>
2017-08-29 09:19:03 -0400

    use fpm image. integrate with nextcloud

--- set_configuration.sh
+++ set_configuration.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-docker exec -u www-data owncloud-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+set -x
 
-if ! grep -q "owncloud-server" trusted_domain.tmp; then
+docker cp ./onlyoffice app-server:/var/www/html/apps/
+docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+
+if ! grep -q "app-server" trusted_domain.tmp; then
 	TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-	docker exec -u www-data owncloud-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="owncloud-server"
+	docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="app-server"
 fi
 
 rm trusted_domain.tmp
 
-docker exec -u www-data owncloud-server php occ --no-warnings app:enable onlyoffice
+docker exec -u www-data app-server php occ --no-warnings app:enable onlyoffice
 
-docker exec -u www-data owncloud-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/"
-docker exec -u www-data owncloud-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data owncloud-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://owncloud-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://app-server/"
