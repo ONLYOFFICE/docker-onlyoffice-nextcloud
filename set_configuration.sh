5b49ee0a301cca6ce211498f285243c15ea78bff
Alexey Bannov <alexey.bannov@onlyoffice.com>
2017-09-04 07:48:04 -0400

    bug fix

--- set_configuration.sh
+++ set_configuration.sh
@@ -7,7 +7,7 @@ docker exec -u www-data app-server php occ --no-warnings config:system:get trust
 
 if ! grep -q "app-server" trusted_domain.tmp; then
 	TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
-	docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="app-server"
+	docker exec -u www-data app-server php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nginx-server"
 fi
 
 rm trusted_domain.tmp
@@ -16,4 +16,4 @@ docker exec -u www-data app-server php occ --no-warnings app:enable onlyoffice
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://app-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
