724acbfb55c7b48feb9103d3d1b187674405a591
Sergey Linnik <sergey.linnik@onlyoffice.com>
2020-06-05 13:11:33 +0300

    Allow local address

--- set_configuration.sh
+++ set_configuration.sh
@@ -16,3 +16,5 @@ docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+
+docker exec -u www-data app-server php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
