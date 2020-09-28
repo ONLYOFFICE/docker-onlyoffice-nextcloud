75b7fdbaa667e206cd1a72af451abac138df45b3
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2020-09-28 12:33:47 +0300

    Fix configuration

--- set_configuration.sh
+++ set_configuration.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ "$1" == "ssl" ]]; then
+	storage_url=https://nginx-server/
+else
+	storage_url=http://nginx-server/
+fi
+
 set -x
 
 docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
@@ -15,6 +21,6 @@ docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="$storage_url"
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
