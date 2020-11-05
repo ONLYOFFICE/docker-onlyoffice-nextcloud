dd5243941e73037946cb8319d89151676187ad2c
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2020-11-05 17:14:52 +0300

    Revert set_configuration.sh

--- set_configuration.sh
+++ set_configuration.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [[ "$1" == "ssl" ]]; then
-	storage_protocol=https
-fi
-
 set -x
 
 docker exec -u www-data app-server php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
@@ -19,6 +15,6 @@ docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
-docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="${storage_protocol:-http}://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
 
 docker exec -u www-data app-server php occ --no-warnings config:system:set allow_local_remote_servers  --value=true
