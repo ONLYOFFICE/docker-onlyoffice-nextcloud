961c1c5a32686467ec56326fec7bcb0ae2ae49d6
Sergey Linnik <sergey.linnik@onlyoffice.com>
2023-04-07 13:09:53 +0500

    enable jwt

--- set_configuration.sh
+++ set_configuration.sh
@@ -16,3 +16,4 @@ docker exec -u www-data app-server php occ --no-warnings app:install onlyoffice
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://onlyoffice-document-server/"
 docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nginx-server/"
+docker exec -u www-data app-server php occ --no-warnings config:system:set onlyoffice jwt_secret --value="secret"
